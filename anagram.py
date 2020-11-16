032ebd95972ed82293b6d1b38be2da5f617bedbc
Nihal Joshi <60462831+joshinihal@users.noreply.github.com>
2020-11-16 20:05:20 +0530

    "initial commit"

---
+++ anagram.py
@@ -0,0 +1,30 @@
+def anagram(s1,s2):
+    
+    s1 = s1.replace(' ','').lower()
+    s2 = s2.replace(' ','').lower()
+    
+    #Edge case check
+    if len(s1) != len(s2):
+        return False
+        
+    count = {}
+    
+    for each in s1:
+        if each in count:
+            count[each] += 1
+        else:
+            count[each] = 1
+            
+    for each in s2:
+        if each in count:
+            count[each] -= 1
+        else:
+            count[each] = 1
+    
+    for k in count:
+        if count[k] != 0:
+            return False
+        
+    return True
+    
+    anagram('Fourth of July','Joyful Fourth')
