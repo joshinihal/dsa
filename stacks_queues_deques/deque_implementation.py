fed4405499b4f8eeae7c8e49def95571313d9c8d
Nihal Joshi <60462831+joshinihal@users.noreply.github.com>
2020-11-22 00:59:44 +0530

    Create deque_implementation.py

---
+++ stacks_queues_deques/deque_implementation.py
@@ -0,0 +1,28 @@
+class Deque(object):
+    
+    def __init__(self):
+        self.items = []
+        
+    def isEmpty(self):
+        return self.items == []
+    
+#   use array in reverse and consider last index as front
+    def addFront(self,item):
+        self.items.append(item)
+        
+#   use array in reverse and consider 0 index as rear
+    def addRear(self,item):
+        self.items.insert(0,item)
+        
+#   use array in reverse and consider last index as front        
+    def removeFront(self):
+        return self.items.pop()
+    
+#   use array in reverse and consider 0 index as rear
+    def removeRear(self):
+        return self.items.pop(0)
+    
+    def size(self):
+        return len(self.items)
+    
+d = Deque()
