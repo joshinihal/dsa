a643d07c205faef50b994bfe773451aa309fa237
Nihal Joshi <60462831+joshinihal@users.noreply.github.com>
2020-11-22 00:32:05 +0530

    Added comments

--- stacks_queues_deques/queue_implementation.py
+++ stacks_queues_deques/queue_implementation.py
@@ -6,9 +6,13 @@ class Queue(object):
     def isEmpty(self):
         return self.items == []
     
+#     enqueue is the process of adding items to the rear,
+#     but use array in reverse and consider 0 index as rear
     def enqueue(self,item):
         self.items.insert(0,item)
         
+#     dequeue is the process of removing items from the front,
+#     but use array in reverse and consider last index as front
     def dequeue(self):
         return self.items.pop()
         
