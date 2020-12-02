7bf7ee6dbbfed02f7c0ef2f86abbfb407eb145dd
Nihal Joshi <60462831+joshinihal@users.noreply.github.com>
2020-12-02 22:11:38 +0530

    Create queue_implementation_with_2_stacks.py

---
+++ stacks_queues_deques/queue_implementation_with_2_stacks.py
@@ -0,0 +1,24 @@
+class Queue2Stacks(object):
+    
+    def __init__(self):
+        
+        # Two Stacks
+        self.instack = []
+        self.outstack = []
+     
+     
+    # Since stack only allows adding and removing from one end,
+    # two stacks can be chained to implement a queue. 
+    # The first stack adds an element from one end, 
+    # the second removes from other
+    def enqueue(self,element):
+        
+        self.instack.append(element)
+    
+    def dequeue(self):
+        
+        if not self.outstack:
+            while self.instack:
+                self.outstack.append(self.instack.pop())
+        return self.outstack.pop()
+        
