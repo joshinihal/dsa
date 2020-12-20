e57c8fedacd35ad42722edd83ef30a4065760a00
Nihal Joshi <60462831+joshinihal@users.noreply.github.com>
2020-12-20 21:49:14 +0530

    Create string_permutation.py

---
+++ recursion/string_permutation.py
@@ -0,0 +1,17 @@
+# Given a string, write a function that uses recursion to output a list of all the possible permutations of that string.
+
+# For example, given s='abc' the function should return ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
+def permute(s):
+    
+    out=[]
+    
+    if len(s)==1:
+        out = [s]
+    else:
+         for i, let in enumerate(s):
+                for perm in permute(s[:i] + s[i+1:]):
+                    out = out + [let + perm]
+    return out
+    
+    
+    permute('abc')
