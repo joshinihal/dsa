408a0615738ea918f7937d1a96089cfdf88bc924
Nihal Joshi <60462831+joshinihal@users.noreply.github.com>
2020-12-27 23:56:35 +0530

    Create tree_implementation_using_oop.py

---
+++ trees/tree_implementation_using_oop.py
@@ -0,0 +1,38 @@
+# Nodes and References Implementation of a Tree
+
+class BinaryTree(object):
+    
+    def __init__(self,rootObj):
+#         root value is also called key
+        self.key = rootObj
+        self.leftChild = None
+        self.rightChild = None
+        
+    def insertLeft(self,newNode):
+        if self.leftChild == None:
+            self.leftChild = BinaryTree(newNode)
+        else:
+            t = BinaryTree(newNode)
+            t.leftChild = self.leftChild
+            self.leftChild = t
+            
+    def insertRight(self,newNode):
+        if self.rightChild == None:
+            self.rightChild = BinaryTree(newNode)
+        else:
+            t = BinaryTree(newNode)
+            t.rightChild = self.rightChild
+            self.rightChild = t
+            
+    def getLeftChild(self):
+        return self.leftChild
+    
+    def getRightChild(self):
+        return self.rightChild
+    
+    def setRootValue(self,obj):
+        self.key = obj
+        
+    def getRootValue(self):
+        return self.key
+        
