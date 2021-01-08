f10da92e185ea596916c8890a5d97b3c218a28a6
Nihal Joshi <60462831+joshinihal@users.noreply.github.com>
2021-01-08 23:59:38 +0530

    Update binary_search_tree_implementation.py

--- trees/binary_search_tree_implementation.py
+++ trees/binary_search_tree_implementation.py
@@ -124,3 +124,23 @@ class BinarySearchTree():
             
     def __delitem__(self, key):
         self.delete(key)
+        
+    def findSuccessor(self):
+        succ = None
+        if self.hasRightChild():
+            succ = self.rightChild.findMin()
+        else:
+            if self.parent:
+                if self.isLeftChild():
+                    succ = self.parent
+                else:
+                    self.parent.rightChild = None
+                    succ = self.parent.findSuccessor()
+                    self.parent.rightChild = self
+        return succ
+    
+    def findMin(self):
+        current = self
+        while current.hasLeftChild():
+            current = current.leftChild
+        return current
