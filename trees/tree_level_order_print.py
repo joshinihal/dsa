29f81afaf9765274f885b44f63e6712bf1ae9636
Nihal Joshi <60462831+joshinihal@users.noreply.github.com>
2021-01-19 23:13:58 +0530

    Update tree_level_order_print.py

--- trees/tree_level_order_print.py
+++ trees/tree_level_order_print.py
@@ -1,3 +1,6 @@
+# Given a binary tree of integers, print it in level order. 
+# The output will contain space between the numbers in the same level, and new line between different levels.
+
 import collections 
 
 class Node():
