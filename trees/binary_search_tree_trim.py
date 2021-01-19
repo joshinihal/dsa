64501ea87cd75f7bfdb2664d43ee5edd74c9d836
Nihal Joshi <60462831+joshinihal@users.noreply.github.com>
2021-01-19 23:13:13 +0530

    Update and rename bst_trim.py to binary_search_tree_trim.py

--- trees/binary_search_tree_trim.py
+++ trees/binary_search_tree_trim.py
@@ -1,3 +1,7 @@
+# Given the root of a binary search tree and 2 numbers min and max,
+# trim the tree such that all the numbers in the new tree are between min and max (inclusive).
+# The resulting tree should still be a valid binary search tree.
+
 def trimBST(tree, minVal, maxVal):
     
     if not tree:
